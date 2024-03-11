97cae120694a3ea84b3c9e40ca62887ea0d4ce90
Isabela Fatima <isabelafatimadelimagx@gmail.com>
2024-03-11 16:58:28 -0300

    Add regex patterns and responsiveness

--- public/API/ApiReq.js
+++ public/API/ApiReq.js
@@ -1,14 +1,11 @@
 import axios from "axios";
-// import process from "node:process"
-// import dotnev from "dotenv"
-// dotnev.config();
 
 const requestOptions = {
   method: 'GET',
   url: 'https://youtube-mp36.p.rapidapi.com/dl',
   params: {},
   headers: {
-    'X-RapidAPI-Key': '11550b8d8bmshfde5d8da57cf2a1p155f1ajsnca28568d2f7d', 
+    'X-RapidAPI-Key': '1b3542e5b3mshf9a4d9f7ba4dedcp18a02djsn8818ff7d1d90',
     'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
   }
 };
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import { fetch } from '../API/ApiReq';
+import { fetch } from '../public/API/ApiReq';
 import youtubeIcon from '/images/youtube.png'
+import Lottie from 'react-lottie';
+import animationData from '../public/lotties/6pQtoicGQ1.json';
 
 function App() {
   const [link, setLink] = useState('');
   const [id, setId] = useState(null);
   const [response, setResponse] = useState(null);
   const [disabled, setDisabled] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(null);
+
+//regex para url
+    function extractVideoId(url) {
+      const regex = /(?:youtu\.be\/|youtube\.com\/(?:.*[&?](?:v|vi)=|embed\/|v\/|tv\/|watch\?(?:.*&)?v=))([^"&?\/\s]{11})/;
+    const match = url.match(regex);
+    return match && match[1];
+}
   
   useEffect(() => {
     if (id) {
@@ -21,7 +31,7 @@ function App() {
             setResponse(res.data);
             clearInterval(interval);
           } else if (res.status === 200 && res.data.status === "fail") {
-            alert("Invalid video ID");
+            setErrorMessage("Invalid video ID");
             setDisabled(false);
             clearInterval(interval);
           }
@@ -39,6 +49,15 @@ function App() {
     }
   }, [response]);
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   return (
     <div className="App">
       <div className='container'>
@@ -51,29 +70,43 @@ function App() {
           type="text"
           placeholder="  enter your youtube link..."
           value={link}
+          required
           onChange={(e) => {
             setLink(e.target.value);
           }}
+          
         />
       </div>
       <div>
-      <span className='example'>Example: <span className='url'>https://www.youtube.com/watch?v=7-U8I4iXW4M</span> </span>
+      <span className='example'>Obs:<span className='url'>Seu download será feito em alguns segundos...</span> </span>
       </div>
 
+      <Lottie 
+	    options={defaultOptions}
+        height={30}
+        width={30}
+      />
+
       <div className='containerBtn'>
+      
         <button id='download'
         onClick={() => {
-          const text = link.split("=")[1];
-          if (text) {
-            setId(text);
-          }
+          const videoId = extractVideoId(link);
+            if (videoId) {
+            setId(videoId);
+        }
         }}
         disabled={disabled}
         className={disabled ? "btn-disabled" : ""}
-      >Download</button>
-      </div>
-
+      >
       
+
+     Download
+        
+        </button>
+      </div>
+      
+      {errorMessage && <div className="error-message">{errorMessage}</div>}
     </div>
   )
 }
